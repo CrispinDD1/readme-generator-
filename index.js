ec8030f4f314725080d7fae27b1b22063041cf1e
Crispin Duarte <CrispinDuarte1@outlook.com>
2024-09-28 18:36:31 -0700

    wrote the code for creating a proffesional readme

--- index.js
+++ index.js
@@ -1,13 +1,86 @@
-// TODO: Include packages needed for this application
+//Includes packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Creates an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'tableOfContents',
+    message: 'Add a table of contents if necessary:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the steps of installation?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'How do you use this application?',
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List any collaborators:',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['ISC', 'MIT', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'badges',
+    message: 'Add any badges you want to include:',
+  },
+  {
+    type: 'input',
+    name: 'feautures',
+    message: 'List the features of your application:',
+  },
+  {
+    type: 'input',
+    name: 'howToContribute',
+    message: 'How can peopl contribute:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide instructions for running tests:',
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log('Success! README.md created.');
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const markdownCont = generateMarkdown(answers);
+    writeToFile('README.md', markdownCont);
+  });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'None') {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/license-${license}-black})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'None') {
+    return '';
+  }
+  return `* [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license === 'None') {
+    return '';
+  }
+  return `## License 
+  This project is licensed under ${license}.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description 
+  ${data.description}
+
+  ## Table of Contents
+  ${data.tableOfContents ? data.tableOfContents : 'No table of contents provided.'}
+  ${renderLicenseLink(data.license)}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Badges 
+  ${data.badges ? data.badges : 'No badges provided.'}
+
+  ## Features
+  ${data.features}
+
+  ## How to Contribute 
+  ${data.howToContribute}
+
+  ## Tests 
+  ${data.tests}
+
+  ${renderLicenseBadge(data.license)}
 `;
 }
 
